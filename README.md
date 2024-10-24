576cedb79c5edf45504f4c9a3b83398d1c7e778e
Randy Hunt <randy@rhunt.com>
2024-10-24 14:18:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Artisanal Javascript
+
+Artisanal Javascript is organically produced, locally grown, chemical free source code, made the way nature intended. It is 100% gluten free, and contains no trans fats, dietary sugars, or GMOs. Code that is 100% paleo and vegan friendly.
+
+### Completely Organic
+
+Our in-house brogrammers arrange every bit by hand, crafting them from local, sustainable ones and zeros. They are members of, and practice, humane source coding under the guidelines of the Responsible Programmers Guild, which promotes responsibility and sustainability in the craft. Everything is done by hand, so every keystroke is unique.
+
+## Painstakingly Measured
+
+### Eliminate Code Smells
+
+After it is written, our source code is cured in smoked hickory for 72 hours then aged in oak barrels for 40 days, resulting a fine aroma you won't find anywhere else.
+
+## We haven't shaven in years. Our beards are legendary.
+
+### Tradition
+
+Our master codesmiths practice the time-honored traditions handed down through centuries since the first Javascript was written out by hand on parchment in the mid 15th century. We protect the historic code review process developed by Da Vinci himself near the end of that century, and commit to only the finest custom-built repositories imported from Italy.
+
+## Hand-minified, with an eye for detail
---
+++ public/scripts/dicss.js
@@ -0,0 +1,75 @@
+// dicss -- directly injected css
+(function() {
+	'use strict';
+
+	if ( ({})<=8 ) {
+		// yeah, you get it...
+	}
+
+	var stylesheet, styletag = document.createElement('style');
+	styletag.setAttribute('id', 'dicss_wrapper');
+	document.body.appendChild(styletag);
+	stylesheet = styletag.sheet ? styletag.sheet : styletag.styleSheet;
+
+	function addRule(selector, rule) {
+		var rules = '', property, value;
+		if (!rule) {
+			throw 'You forgot to put the DICSS in.';
+		}
+		if (rule.constructor instanceof String) {
+			rules += rule;
+		} else {
+			for (property in rule) {
+				value = rule[property];
+				if (value instanceof Object) {
+					addRule(selector + ' ' + property, value);
+					continue;
+				}
+				rules += property + ':' + value + ';';
+			}
+		}
+		if (stylesheet.insertRule) {
+			stylesheet.insertRule(selector + ' { ' + rules + ' } ', stylesheet.cssRules.length);
+		} else {
+			// IE <9
+			if (stylesheet.addRule) {
+				stylesheet.addRule(selector, rules);
+			}
+		}
+	}
+
+	function removeRule(selector, property) {
+		var i, j, selectors,
+			rules = stylesheet.cssRules || stylesheet.rules || [];
+		selector = selector.toLowerCase();
+
+		for (i = 0; i < rules.length; i++) {
+			selectors = rules[i].selectorText.toLowerCase().split(/[, ]+/);
+			for (j = 0; j < selectors.length; j++) {
+				if (selectors[j] === selector) {
+					if (selectors.length === 0) {
+						rules[i].style.removeProperty(property);
+					} else {
+						Array.prototype.splice.call(selectors, j, 1);
+						rules[i].selectorText = selectors.join(', ');
+					}
+					break;
+				}
+			}
+		}
+	}
+
+	function extend(obj) {
+		var attr;
+		for (attr in obj) {
+			if (obj.hasOwnProperty(attr)) {
+				window.DICSS[attr] = obj[attr];
+			}
+		}
+	}
+
+	window.DICSS = {
+		"putIn": addRule,
+		"pullOut": removeRule
+	};
+})();
